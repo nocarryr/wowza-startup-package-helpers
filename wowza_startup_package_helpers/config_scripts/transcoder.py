eb6f4c9e7a919f2222ac48921d23874d12318ccb
nocarryr <matt@nomadic-recording.com>
2015-02-06 14:03:23 -0600

    more transcoding stuff

--- wowza_startup_package_helpers/config_scripts/transcoder.py
+++ wowza_startup_package_helpers/config_scripts/transcoder.py
@@ -7,31 +7,59 @@ class Transrate(ParsedXMLFile):
         self.script = kwargs.get('script')
         kwargs['filename'] = self.script.build_filename('transcoder', 'templates', 'transrate.xml')
         super(Transrate, self).__init__(**kwargs)
+        
 class TranscodeMod(object):
     def __init__(self, **kwargs):
         self.script = kwargs.get('script')
         self.name = kwargs.get('name')
         self.base_path = '/'.join(['Root/Transcode', self.search_path])
         self.xml_file = self.script.transrate_xml
+        self.parent = kwargs.get('parent')
         self.node = self.find_node()
     def find_node(self):
-        p = '/'.join([self.base_path, self.tag, 'Name'])
-        for match in self.xml_file.find_by_path(p, single_result=False):
+        if self.parent is not None:
+            obj = self.parent.node
+        else:
+            obj = self.xml_file
+        p = '/'.join([self.base_path, self.tag])
+        if self.name is None:
+            return obj.find_by_path(p)
+        p = '/'.join([p, 'Name'])
+        for match in obj.find_by_path(p, single_result=False):
             if match.text == self.name:
                 return match
-                
+    def build_node(self, **kwargs):
+        return DictNode(**kwargs)
+        
 class Encode(TranscodeMod):
     search_path = 'Encodes'
     tag = 'Encode'
     def __init__(self, **kwargs):
         super(Encode, self).__init__(**kwargs)
         self.enabled = kwargs.get('enabled')
+        vkwargs = kwargs.get('video', {})
+        vkwargs.update(dict(script=self.script, parent=self))
+        self.video = EncodeVideo(**vkwargs)
+        akwargs = kwargs.get('audio', {})
+        akwargs.update(dict(script=self.script, parent=self))
+        self.audio = EncodeAudio(**akwargs)
+        
+class EncodeVideo(TranscodeMod):
+    tag = 'Video'
+    def __init__(self, **kwargs):
+        super(EncodeVideo, self).__init__(**kwargs)
         self.width = kwargs.get('width')
         self.height = kwargs.get('height')
         self.profile = kwargs.get('profile')
         self.bitrate = kwargs.get('bitrate')
-        self.aud_codec = kwargs.get('aud_codec')
-        self.aud_bitrate = kwargs.get('aud_bitrate')
+        
+class EncodeAudio(TranscodeMod):
+    tag = 'Audio'
+    def __init__(self, **kwargs):
+        super(EncodeAudio, self).__init__(**kwargs)
+        self.codec = kwargs.get('codec')
+        self.bitrate = kwargs.get('bitrate')
+        
         
 class StreamNameGroup(TranscodeMod):
     def __init__(self, **kwargs):
