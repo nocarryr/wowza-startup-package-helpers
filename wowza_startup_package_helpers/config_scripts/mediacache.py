fc28f49685be7e8a13fc68212090a7a5b930bd65
nocarryr <matt@nomadic-recording.com>
2015-02-06 15:28:20 -0600

    add aws credentials to mediacache

--- wowza_startup_package_helpers/config_scripts/mediacache.py
+++ wowza_startup_package_helpers/config_scripts/mediacache.py
@@ -1,4 +1,18 @@
 from base_script import BaseScript
+from wowza_startup_package_helpers.xml_handlers.hanlder import ParsedXMLFile
 
 class Script(BaseScript):
     _conf_vars = ['aws_access_key_id', 'aws_secret_access_key']
+    def __call__(self):
+        fn = self.build_conf_filename('MediaCache.xml')
+        xml_file = ParsedXMLFile(filename=fn)
+        props_node = xml_file.find_by_path('Properties')
+        for name_node in props_node.find_by_path('Properties/Property/Name'):
+            if 'aws' not in name_node.text:
+                continue
+            value_node = name_node.parent.find_by_path('Property/Value')[0]
+            if 'accesskeyid' in name_node.text.lower():
+                value_node.text = self.config.aws_access_key_id
+            elif 'secretaccesskey' in name_node.text.lower():
+                value_node.text = self.config.aws_secret_access_key
+        xml_file.write()
