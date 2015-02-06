8d9ac1366f993d17aa63062679db3529d1f92d15
nocarryr <matt@nomadic-recording.com>
2015-02-06 12:41:44 -0600

    xml handling progress mostly complete

--- wowza_startup_package_helpers/xml_handlers/handler.py
+++ wowza_startup_package_helpers/xml_handlers/handler.py
@@ -4,16 +4,53 @@ from wowza_startup_package_helpers.xml_handlers.objects import XMLNode, DictNode
 
 class XMLDoc(object):
     def __init__(self, **kwargs):
-        pass
+        self._tree = None
+        self._root_node = None
+    @property
+    def tree(self):
+        return self._tree
+    @tree.setter
+    def tree(self, value):
+        self._tree = value
+    @property
+    def root_node(self):
+        return self._root_node
+    @root_node.setter
+    def root_node(self, value):
+        self._root_node = value
+    def find_by_path(self, path, single_result=True):
+        match = self.root_node.find_by_path(path)
+        if not len(match):
+            raise 'No match found for %s' % (path)
+        if single_result:
+            if len(match) > 1:
+                raise 'Multiple matches for %s' % (path)
+            match = match[0]
+        return match
+    def insert_from_dict(self, d, path):
+        parent = self.find_by_path(path)
+        dnode = DictNode(**d)
+        dnode.to_xml_node(parent=parent)
+    def insert_from_json(self, js_str, path):
+        parent = self.find_by_path(path)
+        dnode = DictNode.from_json(js_str)
+        dnode.to_xml_node(parent=parent)
         
 class XMLFile(XMLDoc):
     def __init__(self, **kwargs):
         super(XMLFile, self).__init__(**kwargs)
         self.filename = kwargs.get('filename')
+    def write(self):
+        root = self.root_node
+        root.prepare_print()
+        t = self.tree
+        if t is None:
+            t = ElementTree.ElementTree(element=root.node)
+        t.write(self.filename)
         
 class ParsedXMLFile(XMLFile):
     def __init__(self, **kwargs):
         super(ParsedXMLFile, self).__init__(**kwargs)
-        tree = ElementTree.parse(self.filename)
-        root = tree.getroot()
+        self.tree = ElementTree.parse(self.filename)
+        root = self.tree.getroot()
         self.root_node = XMLNode(node=root)
--- wowza_startup_package_helpers/xml_handlers/objects.py
+++ wowza_startup_package_helpers/xml_handlers/objects.py
@@ -2,7 +2,12 @@ from xml.etree import ElementTree
 import json
 
 class BaseNode(object):
+    _path_sep = '/'
     def __init__(self, **kwargs):
+        self._path = None
+        self._tag = None
+        self._text = None
+        self.attribs = {}
         self.parent = kwargs.get('parent')
         self.children = []
     @property
@@ -10,11 +15,71 @@ class BaseNode(object):
         if self.parent is None:
             return self
         return self.parent.root
+    @property
+    def path(self):
+        parent = self.parent
+        if parent is None:
+            p = self.tag
+        else:
+            p = self._path_sep.join([parent.path, self.tag])
+        if p != self._path:
+            self._path = p
+        return p
+    @property
+    def depth(self):
+        p = self.parent
+        if p is None:
+            return 0
+        return p.depth + 1
+    @property
+    def tag(self):
+        return self._get_tag()
+    @tag.setter
+    def tag(self, value):
+        self._set_tag(value)
+    @property
+    def text(self):
+        return self._get_text()
+    @text.setter
+    def text(self, value):
+        self._set_text(value)
+    def _get_tag(self):
+        return self._tag
+    def _set_tag(self, value):
+        if value == self._tag:
+            return
+        self._tag = value
+    def _get_text(self):
+        return self._text
+    def _set_text(self, value):
+        if value == self._text:
+            return
+        self._text = value
+    def get_attrib(self, key, default=None):
+        return self.attribs.get(key, default)
+    def set_attrib(self, key, val):
+        self.attribs[key] = val
+    def update_attribs(self, attribs):
+        self.attribs.update(attribs)
     def add_child(self, **kwargs):
         kwargs['parent'] = self
         obj = self.__class__(**kwargs)
         self.children.append(obj)
         return obj
+    def find_by_path(self, p):
+        my_path = self.path
+        path_sep = self._path_sep
+        if p == my_path:
+            return [self]
+        if p.split(path_sep)[0] != my_path.split(path_sep)[0]:
+            return None
+        matches = []
+        for child in self.children:
+            match = child.find_by_path(p)
+            if match is None:
+                continue
+            matches.extend(match)
+        return matches
         
 class XMLNode(BaseNode):
     def __init__(self, **kwargs):
@@ -28,7 +93,7 @@ class XMLNode(BaseNode):
                 self.node = ElementTree.SubElement(self.parent.node, 'a')
             self.tag = kwargs.get('tag')
             for key, val in kwargs.get('attrib', {}).iteritems():
-                self.node.set(key, val)
+                self.set_attrib(key, val)
             if 'text' in kwargs:
                 self.text = kwargs['text']
             for child in kwargs.get('children', []):
@@ -45,32 +110,53 @@ class XMLNode(BaseNode):
             return
         for c in node:
             self.add_child(node=c)
-    @property
-    def tag(self):
-        if self.node is None:
-            return None
-        return self.node.tag
-    @tag.setter
-    def tag(self, value):
-        if self.node is None:
-            return
-        self.node.tag = value
-    @property
-    def text(self):
-        if self.node is None:
-            return None
-        t = self.node.text
-        if t is None:
+    def _get_tag(self):
+        if self.node is not None:
+            t = self.node.tag
+            if t != self._tag:
+                self._tag = t
             return t
-        for char in ['\n', '\t']:
-            t = ''.join(t.split(char))
-        t = t.strip(' ')
-        return t
-    @text.setter
-    def text(self, value):
+        return self._tag
+    def _set_tag(self, value):
+        if self.node is not None:
+            self.node.tag = value
+        self._tag = value
+    def _get_text(self):
         if self.node is None:
-            return
-        self.node.text = value
+            t = self._text
+        else:
+            t = self.node.text
+        if isinstance(t, basestring):
+            if '\n' in t or '\t' in t:
+                for char in ['\n', '\t']:
+                    t = ''.join(t.split(char))
+                t = t.strip(' ')
+                if self.node is not None:
+                    self.node.text = t
+                    self.node.tail = ''
+            self._text = t
+        return t
+    def _set_text(self, value):
+        super(XMLNode, self)._set_text(value)
+        if self.node is not None:
+            self.node.text = value
+    def get_attrib(self, key, default=None):
+        if self.node is not None:
+            return self.node.get(key, default)
+        return super(XMLNode, self).get_attrib(key, default)
+    def set_attrib(self, key, val):
+        if self.node is not None:
+            self.node.set(key, val)
+        super(XMLNode, self).set_attrib(key, val)
+    def prepare_print(self):
+        d = self.depth
+        p = self.parent
+        if p is not None:
+            if not p.text:
+                p.text = '\n' + '\t' * d
+            self.node.tail = '\n' + '\t' * p.depth
+        for c in self.children:
+            c.prepare_print()
     def __str__(self):
         return str(self.node)
         
@@ -78,7 +164,7 @@ class DictNode(BaseNode):
     def __init__(self, **kwargs):
         super(DictNode, self).__init__(**kwargs)
         self.tag = kwargs.get('tag')
-        self.attribs = kwargs.get('attribs', {})
+        self.update_attribs(kwargs.get('attribs', {}))
         self.text = kwargs.get('text')
         for child in kwargs.get('children', []):
             self.add_child(**child)
@@ -88,8 +174,11 @@ class DictNode(BaseNode):
         for child in self.children:
             d['children'].append(child.to_dict())
         return d
-    def to_xml_node(self):
+    def to_xml_node(self, **kwargs):
         d = self.to_dict()
+        p = kwargs.get('parent')
+        if p is not None:
+            return p.add_child(**d)
         return XMLNode(**d)
     @classmethod
     def from_json(cls, js_str):
